51b7dea4ead250e7b79ffe3902a2b941a88c74b7
Hoooooou-Jun <jipkim2@gmail.com>
2023-07-07 16:20:38 +0900

    메인 컬러 정의

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,4 +1,7 @@
 export enum EColor {
+  COLOR_PRIMARY = "#07A37E",
+  COLOR_SECONDARY = "#00785C",
+  COLOR_INTERACTION = "#EBFFF0",
   TEXT_900 = "#1A1A1A",
   TEXT_800 = "#262729",
   TEXT_700 = "#858585",
@@ -7,4 +10,6 @@ export enum EColor {
   TEXT_400 = "#F3F2F0",
   TEXT_300 = "#F7F7F7",
   TEXT_200 = "#FFFFFF",
+  GRAD_START = "#22859B",
+  GRAD_END = "#80FF95",
 }
