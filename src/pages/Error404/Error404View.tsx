f605c69977bb5961a80b3eb3510bbadc6815b9b4
LSH <0867sh@naver.com>
2023-09-18 16:56:31 +0900

    404페이지

---
+++ src/pages/Error404/Error404View.tsx
@@ -0,0 +1,21 @@
+import { Body, Container, StyledImage, TitleSubText, TitleText } from "./styles";
+import logo512 from '@assets/images/logo512.png';
+
+const Error404View = () => {
+  return (
+    <Container>
+      <StyledImage src={logo512} />
+      <TitleText>
+        Hansei Weekly Menu
+      </TitleText>
+      <TitleSubText>
+        한세대학교 주간식단표 서비스
+      </TitleSubText>
+      <Body>
+          404 Not Found<br/>페이지를 찾을 수 없습니다.
+      </Body>
+    </Container>
+  );
+}
+
+export default Error404View;
