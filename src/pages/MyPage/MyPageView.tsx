f44181de87c5f3ab5b7aa4382bd47e51c5a72d9e
Hoooooou-Jun <jipkim2@gmail.com>
2023-11-26 20:50:47 +0900

    페이지 연결

--- src/pages/MyPage/MyPageView.tsx
+++ src/pages/MyPage/MyPageView.tsx
@@ -1,14 +1,23 @@
-import {useRecoilValue} from 'recoil';
+import {useRecoilState, useRecoilValue} from 'recoil';
 
-import {langState} from '@modules/atoms';
+import {langState, userState} from '@modules/atoms';
 import {MyPageString} from '@utils/constants/strings';
 import {Container, Logo} from './styles';
 import PageLogo from '@components/PageLogo';
 import {Divider} from '@components/Divider';
 import {ListButton} from '@components/ListButton';
+import { User } from '@type/index';
+import { useNavigate } from 'react-router-dom';
 
 const MyPageView = () => {
+  const navigate = useNavigate();
   const lang = useRecoilValue(langState);
+  const [{page}, set_page] = useRecoilState<User>(userState);
+
+  const handleNavigate = (name: string) => {
+    set_page({ page: name });
+    navigate(`/${name}`);
+  };
 
   return (
     <Container>
@@ -16,15 +25,15 @@ const MyPageView = () => {
         title={MyPageString({lang: lang, key: 'title'})}
         subtitle={''}
       />
-      <ListButton label={'로그인'} />
+      <ListButton label={'로그인'} onClick={() => handleNavigate('login')} />
       <Divider />
       <div>
         <ListButton label={'구매 내역'} />
       </div>
       <Divider />
       <div>
-        <ListButton label={'도움말'} />
-        <ListButton label={'About me'} />
+        <ListButton label={'도움말'} onClick={() => handleNavigate('help')} />
+        <ListButton label={'About me'} onClick={() => handleNavigate('about-me')} />
       </div>
     </Container>
   );
