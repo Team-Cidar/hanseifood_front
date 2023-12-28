894c51fcc126322e5a71df9b0a4095ff8c695a67
LSH <0867sh@naver.com>
2023-12-28 15:33:24 +0900

    로그인 유뮤 확인 기능 추가

--- src/pages/MyPage/MyPageView.tsx
+++ src/pages/MyPage/MyPageView.tsx
@@ -1,6 +1,6 @@
 // MyPageView.tsx
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Logo, Button } from './styles';
 import { Link } from 'react-router-dom';
 import {useRecoilState, useRecoilValue} from 'recoil';
@@ -12,17 +12,39 @@ import {Divider} from '@components/Divider';
 import {ListButton} from '@components/ListButton';
 import {User} from '@type/index';
 import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
 
 const MyPageView = () => {
   const navigate = useNavigate();
   const lang = useRecoilValue(langState);
   const [{page}, set_page] = useRecoilState<User>(userState);
+  const [Login, setLogin] = useState('False');
+
+  useEffect(() => {
+    const data = {"token": localStorage.getItem('accessToken')}
+    // 페이지 최초 렌더링 시에 실행될 코드
+    const verifyToken = async () => {
+      try {
+        const response = await axios.post('http://localhost:8000/api/token/verify',data)
+
+        if (response.status === 200) {
+          setLogin('True')
+        } else {
+          setLogin('False')
+        }
+      } catch (error) {
+        setLogin('False')
+      }
+    };
+
+    verifyToken();
+  }, []);
 
   const handleNavigate = (name: string) => {
     set_page({page: name});
     navigate(`/${name}`);
   };
-
+  
   return (
     <Container>
       <Button as={Link} to="/back-office">
@@ -32,10 +54,21 @@ const MyPageView = () => {
         title={MyPageString({lang: lang, key: 'title'})}
         subtitle={''}
       />
+      {Login === 'False' ? (
       <ListButton
         label={MyPageString({lang: lang, key: 'listbutton.label.login'})}
         onClick={() => handleNavigate('login')}
       />
+      ) : (
+        <ListButton
+        label={MyPageString({lang: lang, key: 'listbutton.label.logout'})}
+        onClick={() => {if (confirm("정말 로그아웃 하시겠습니까?") == true){
+          localStorage.clear();
+          window.location.href = "https://kauth.kakao.com/oauth/logout?client_id=c8300aee5549fc7db67a25a714144789&logout_redirect_uri=http://localhost:8080";
+        }}}
+      />
+      )
+      }
       <Divider />
       <div>
         <ListButton
