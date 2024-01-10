c1e516412afc107272263eac618a3df2f3a11091
Hoooooou-Jun <jipkim2@gmail.com>
2024-01-10 20:41:57 +0900

    feature, test: 헤더바 구현, 댓글 테스트 래이아웃 작성

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -12,6 +12,7 @@ import {useRecoilState} from 'recoil';
 import {User} from '@type/index';
 import {userState} from '@modules/atoms';
 import {EColor} from '@styles/color';
+import { HeaderBar } from '@components/HeaderBar';
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -25,42 +26,51 @@ const Navbar = () => {
     navigate(`/${name}`);
   };
 
-  return (
-    <Container>
-      <NavbarBox>
-        <NavbarItem onClick={() => handleNavigate('home')}>
-          {page === 'home' ? (
-            <SvgIcon name={'home_on'} width={78} height={70} fill={''} />
-          ) : (
-            <SvgIcon name={'home'} width={78} height={70} fill={''} />
-          )}
-        </NavbarItem>
-        <NavbarItem onClick={() => handleNavigate('ticket')}>
-          {page === 'ticket' ? (
-            <TicketBoxOn>
-              <SvgIcon
-                name={'ticket_on'}
-                width={34}
-                height={28}
-                fill={EColor.COLOR_INTERACTION}
-              />
-            </TicketBoxOn>
-          ) : (
-            <TicketBox>
-              <SvgIcon name={'ticket'} width={34} height={28} fill={''} />
-            </TicketBox>
-          )}
-        </NavbarItem>
-        <NavbarItem onClick={() => handleNavigate('mypage')}>
-          {page === 'mypage' ? (
-            <SvgIcon name={'mypage_on'} width={78} height={70} fill={'white'} />
-          ) : (
-            <SvgIcon name={'mypage'} width={78} height={70} fill={'white'} />
-          )}
-        </NavbarItem>
-      </NavbarBox>
-    </Container>
-  );
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  if (page === "home" || page === "ticket" || page === "mypage") {
+    return (
+      <Container>
+        <NavbarBox>
+          <NavbarItem onClick={() => handleNavigate('home')}>
+            {page === 'home' ? (
+              <SvgIcon name={'home_on'} width={78} height={70} fill={''} />
+            ) : (
+              <SvgIcon name={'home'} width={78} height={70} fill={''} />
+            )}
+          </NavbarItem>
+          <NavbarItem onClick={() => handleNavigate('ticket')}>
+            {page === 'ticket' ? (
+              <TicketBoxOn>
+                <SvgIcon
+                  name={'ticket_on'}
+                  width={34}
+                  height={28}
+                  fill={EColor.COLOR_INTERACTION}
+                />
+              </TicketBoxOn>
+            ) : (
+              <TicketBox>
+                <SvgIcon name={'ticket'} width={34} height={28} fill={''} />
+              </TicketBox>
+            )}
+          </NavbarItem>
+          <NavbarItem onClick={() => handleNavigate('mypage')}>
+            {page === 'mypage' ? (
+              <SvgIcon name={'mypage_on'} width={78} height={70} fill={'white'} />
+            ) : (
+              <SvgIcon name={'mypage'} width={78} height={70} fill={'white'} />
+            )}
+          </NavbarItem>
+        </NavbarBox>
+      </Container>
+    );
+  } else {
+    return <HeaderBar left={<SvgIcon name={"back"} width={24} height={24} fill={"none"} />} onClickLeft={handleBack} />;
+  }
+
 };
 
 export default Navbar;
